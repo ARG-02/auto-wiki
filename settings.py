098e55102f8b0df5b4e49c7b98b2943dcf3b2206
Adit Agarwal <aditagarwal76@gmail.com>
2022-05-10 20:20:16 -0500

    Edited notebook file

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # ----- Settings ----- #
 article_names = [u"Influenza", u"Panama", u"Algorithm", u"Badminton", u"Rice", u"Dog", u"Dragon", u"Scarlet_Johansson", u"Taco_Bell", u"Kidney"]  # Name of the articles to be generated
-header_requirement = 5  # Max number of headers that can appear in an article
+header_min_bound = 2  # Precision and Recall minimum heading amount to generate
 initial_similar_article_count = 20  # The number of articles which are initially searched to be similar
 similar_articles_cutoff = 7  # Max number of similar articles that can be used
 similarity_threshold = 0.2  # Necessary cosine similarity for similar articles
@@ -11,3 +11,4 @@ blacklisted_categories = ['references', 'see also', 'external links', 'further r
 wikipedia_model_file = 'wikipedia2vec_model.pkl'  # File name/location of wikipedia2vec model
 sentence_transformers_model = 'whaleloops/phrase-bert'  # Model for sentence_transformers (recommended: 'whaleloops/phrase-bert', 'sentence-transformers/all-MiniLM-L6-v2')
 headers_output_text_name = "Article Headers Evaluation.txt"  # File name/location of headers output information txt
+pr_curve_graphs = "pr_curves"  # Directory name / path for the pr_curve graphs to save too
